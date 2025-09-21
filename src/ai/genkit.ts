d898a0d990734f15b68bbdfaabd6b96db89861af
Harsh Raj <harshraj84068@gmail.com>
2025-09-21 18:54:31 +0000

    yoo give option to enter api key not create projecy

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,7 +3,7 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 import { config } from 'dotenv';
 
-config();
+config({ path: '.env' });
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
